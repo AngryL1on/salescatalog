1821b88fbe1ff87860bcff8d469c9a0beb4d4503
AngryL1on <95445205+AngryL1on@users.noreply.github.com>
2023-10-09 15:10:28 +0300

    Initial commit

---
+++ src/main/java/ru/rutmiit/salescatalog/SalescatalogApplication.java
@@ -0,0 +1,13 @@
+package ru.rutmiit.salescatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalescatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalescatalogApplication.class, args);
+	}
+
+}
